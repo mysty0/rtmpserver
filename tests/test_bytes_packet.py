a86e5c4898bc52186728000ebb73da3a33890fe1
Yaroslav <maplegend@mail.ru>
2020-02-12 13:41:44 +0000

    Added write tests

--- tests/test_bytes_packet.py
+++ tests/test_bytes_packet.py
@@ -26,3 +26,43 @@ def test_get():
     pack = BytesPacket(data)
     assert pack.pop_u8() == 1
     assert pack.get(0) == 2
+
+
+def test_string_pop():
+    data = struct.pack('!h4s', 4, 'test'.encode('utf-8'))
+    pack = BytesPacket(data)
+    assert pack.pop_string() == 'test'
+
+
+def test_write_u8():
+    pack = BytesPacket(bytearray())
+    pack.push_u8(23)
+    assert len(pack) == 1
+    assert pack.pop_u8() == 23
+
+
+def test_write_u16():
+    pack = BytesPacket(bytearray())
+    pack.push_u16(23)
+    assert len(pack) == 2
+    assert pack.pop_u16() == 23
+
+
+def test_write_u32():
+    pack = BytesPacket(bytearray())
+    pack.push_u32(23)
+    assert len(pack) == 4
+    assert pack.pop_u32() == 23
+
+
+def test_write_u64():
+    pack = BytesPacket(bytearray())
+    pack.push_u64(23)
+    assert len(pack) == 8
+    assert pack.pop_u64() == 23
+
+
+def test_write_string():
+    pack = BytesPacket(bytearray())
+    pack.push_string('testtest')
+    assert pack.pop_string() == 'testtest'
